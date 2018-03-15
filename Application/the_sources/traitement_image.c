ad1c3abf68a9a719fb4115802da20e58c9a3742d
Abyssion <alexis@lattard.fr>
2018-03-15 16:29:35 +0100

    Mise a plat

--- Application/the_sources/traitement_image.c
+++ Application/the_sources/traitement_image.c
@@ -8,25 +8,15 @@ image_s* lire_ppm(FILE* f){
   fscanf(f,"%s %d %d %d",image->type,&(image->largeur),&(image->hauteur),&(image->val_max));
   printf("%s %d %d %d\n",image->type,image->largeur,image->hauteur,image->val_max );
 }
-/*
-uint64_t mask(uint64_t, unsigned int longueur, unsigned int debut) {
-  for
-}
 
 void to_grey_level(image_s* image, unsigned float a, unsigned float b, unsigned float c){
 
 }
 
 void to_binary(image_s* image, unsigned float alpha){
-  for (int i = 0; i < count; i++) {
-    if ((image->pixels[i])/pow(image->val_max, 3) < alpha) {
-      image->pixels[i] = 1;
-    } else {
-      image->pixels[i] = 0;
-    }
-  }
+
 }
-*/
+
 void to_pgm(image_s* image){
 
 }
