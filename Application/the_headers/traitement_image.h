eb52b042d9f7f530e485a627a34d5c0ab4cc622a
Abyssion <alexis@lattard.fr>
2018-03-15 15:12:11 +0100

    Ajout initiaux

---
+++ Application/the_headers/traitement_image.h
@@ -0,0 +1,16 @@
+#ifndef TRAITEMENT_H
+#define TRAITEMENT_H
+
+typedef enum image_type image_type{
+  P1 = 1, P2 = 2, P3 = 3
+};
+
+typedef struct {
+  image_type type;
+  uint32_t largeur;
+  uint32_t hauteur;
+  unsigned int val_max;
+  uint64_t* pixels;
+}image_s;
+
+#endif
